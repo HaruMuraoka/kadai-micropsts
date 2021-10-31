002404abd6aac7d068dfafe50e5b9d6b5e04599c
haru.muraoka <haru.3151@icloud.com>
2021-10-31 12:58:19 +0000

    feed_microposts

--- app/User.php
+++ app/User.php
@@ -100,7 +100,7 @@ class User extends Authenticatable
     public function feed_microposts()
     {
         // このユーザがフォロー中のユーザのidを取得して配列にする
-        $userIds = $this->foolowings()->pluck('users.id')->toArray();
+        $userIds = $this->followings()->pluck('users.id')->toArray();
         // このユーザのidもその配列に追加
         $userIds[] = $this->id;
         // それらのユーザが所有する投稿に絞り込む
--- resources/views/user_follow/follow_button.blade.php
+++ resources/views/user_follow/follow_button.blade.php
@@ -7,7 +7,7 @@
     @else
         {{-- フォローボタンのフォーム --}}
         {!! Form::open(['route' => ['user.follow', $user->id]]) !!}
-            {!! Form::submit('follow', ['class' => "btn btn-primary btn-block"]) !!}
+            {!! Form::submit('Follow', ['class' => "btn btn-primary btn-block"]) !!}
         {!! Form::close() !!}
     @endif
 @endif
